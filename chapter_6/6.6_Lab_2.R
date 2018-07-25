cf4006355598cedc8c3ace209f4f23fb75d170fa
Marshall McQuillen <marshallm94@gmail.com>
2018-07-25 06:40:14 -0600

    Add Lab 2: Lasso

--- chapter_6/6.6_Lab_2.R
+++ chapter_6/6.6_Lab_2.R
@@ -65,3 +65,22 @@ mean((ridge.pred - y.test)^2)
 
 out <- glmnet(x, y, alpha = 0)
 predict(out, type = 'coefficients', s = bestlam)[1:20,]
+
+# The Lasso
+lasso.mod <- glmnet(x[train, ], y[train], alpha = 1, lambda = grid)
+plot(lasso.mod)
+
+set.seed(1)
+cv.out <- cv.glmnet(x[train, ], y[train], alpha = 1)
+plot(cv.out)
+bestlam <- cv.out$lambda.min
+lasso.pred <- predict(lasso.mod, s = bestlam, newx = x[test, ])
+mean((lasso.pred - y.test)^2)
+
+out <- glmnet(x, y, alpha = 1, lambda = grid)
+lasso.coef <- predict(out,
+                      type = 'coefficients',
+                      s = bestlam,
+                      newx = x[test, ])[1:20,]
+lasso.coef
+lasso.coef[lasso.coef != 0]
