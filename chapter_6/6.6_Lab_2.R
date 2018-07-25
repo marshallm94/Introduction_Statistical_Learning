6bd3c772372ebf300c49c9e2ecbd15c5c3cd6379
Marshall McQuillen <marshallm94@gmail.com>
2018-07-25 06:30:57 -0600

    Add Lab 2: Ridge Regression

---
+++ chapter_6/6.6_Lab_2.R
@@ -0,0 +1,67 @@
+# Setup
+library(ISLR)
+x <- model.matrix(Salary ~ ., data = Hitters)[, -1]
+y <- na.omit(Hitters$Salary)
+
+# Ridge Regression
+library(glmnet)
+# set lambda grid
+grid <- 10^seq(10, -2, length = 100)
+ridge.mod <- glmnet(x, y, alpha = 0, lambda = grid)
+
+dim(coef(ridge.mod))
+
+ridge.mod$lambda[50]
+coef(ridge.mod)[,50]
+sqrt(sum(coef(ridge.mod)[-1,50]^2))
+
+ridge.mod$lambda[60]
+coef(ridge.mod)[,60]
+sqrt(sum(coef(ridge.mod)[-1,60]^2))
+
+predict(ridge.mod, s = 50, type = 'coefficients')[1:20,]
+
+set.seed(1)
+train <- sample(1:nrow(x), nrow(x)/2)
+test <- (-train)
+y.test <- y[test]
+ridge.mod <- glmnet(x[train,],
+                    y[train],
+                    alpha = 0,
+                    lambda = grid,
+                    thresh = 1e-12)
+ridge.pred <- predict(ridge.mod, s = 4, newx = x[test, ])
+mean((ridge.pred - y.test)^2)
+# intercept-only mondel
+mean((mean(y[train]) - y.test)^2)
+# large lambda approaches intercept-only model
+ridge.pred <- predict(ridge.mod, s=1e10, newx = x[test, ])
+mean((ridge.pred - y.test)^2)
+
+# least squares model
+ridge.pred <- predict(ridge.mod,
+                      x=x[train,],
+                      y=y[train],
+                      s = 0,
+                      newx = x[test, ],
+                      exact = T)
+mean((ridge.pred - y.test)^2)
+lm(y ~ x, subset = train)
+predict(ridge.mod,
+        x = x[train,],
+        y = y[train],
+        s = 0,
+        exact = T,
+        type = 'coefficients')[1:20,]
+
+set.seed(1)
+cv.out <- cv.glmnet(x[train, ], y[train], alpha = 0)
+plot(cv.out)
+bestlam <- cv.out$lambda.min
+bestlam
+
+ridge.pred <- predict(ridge.mod, s = bestlam, newx = x[test, ])
+mean((ridge.pred - y.test)^2)
+
+out <- glmnet(x, y, alpha = 0)
+predict(out, type = 'coefficients', s = bestlam)[1:20,]
