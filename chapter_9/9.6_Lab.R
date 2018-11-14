90d6162773d97cfbc8d5885a6438ad87ddbe0425
Marshall McQuillen <Marshall.McQuillen@ihsmarkit.com>
2018-11-14 05:19:27 -0700

    Add complete chapter 9 lab

--- chapter_9/9.6_Lab.R
+++ chapter_9/9.6_Lab.R
@@ -179,16 +179,7 @@ for (i in 1:length(thresholds)) {
     fp.rate[i] <- 1 - cm$byClass['Specificity']
 }
 
-roc_df <- data.frame(x = fp.rate, y = tp.rate)
-
-# roc plot
-ggplot(roc_df, aes(x, y)) +
-    geom_line(color = 'blue') +
-    ggtitle("Support Vector Machine Training ROC",
-            "Positive class = 2") +
-    xlab("False Positive Rate (1 - Specificity)") +
-    ylab("True Positive Rate (Sensitivity)")
-
+roc_df <- data.frame(fpr_gamma_2 = fp.rate, tpr_gamma_2 = tp.rate)
 
 # increase gamma
 svm.opt <- svm(y ~ .,
@@ -215,12 +206,101 @@ for (i in 1:length(thresholds)) {
     fp.rate[i] <- 1 - cm$byClass['Specificity']
 }
 
-roc_df <- data.frame(x = fp.rate, y = tp.rate)
+roc_df$fpr_gamma_50 <- fp.rate
+roc_df$tpr_gamma_50 <- tp.rate
 
 # roc plot
-ggplot(roc_df, aes(x, y)) +
-    geom_line(color = 'blue') +
+ggplot(roc_df, aes(fpr_gamma_2, tpr_gamma_2)) +
+    geom_line(aes(color = '2')) +
+    geom_line(aes(fpr_gamma_50, tpr_gamma_50, color = '50')) +
     ggtitle("Support Vector Machine Training ROC",
             "Positive class = 2") +
     xlab("False Positive Rate (1 - Specificity)") +
     ylab("True Positive Rate (Sensitivity)")
+
+# apply to testing data
+
+svm.opt <- svm(y ~ .,
+               data = dat[train, ],
+               kernel = 'radial',
+               gamma = 2,
+               cost = 1,
+               probability = TRUE) 
+
+y.hat.prob <- attr(predict(svm.opt,
+                           newdata = dat[-train, ],
+                           probability = TRUE), "probabilities")
+
+# get true and false positive rates for varying thresholds
+thresholds <- seq(0.01, 1, 0.01)
+tp.rate <- rep(0, length(thresholds))
+fp.rate <- rep(0, length(thresholds))
+
+for (i in 1:length(thresholds)) {
+    y.hat <- ifelse(y.hat.prob[, 1] > thresholds[i], '2', '1')
+    cm <- confusionMatrix(data = factor(y.hat, levels = 1:2),
+                          reference = factor(dat[-train, 'y'], levels = 1:2))
+    tp.rate[i] <- cm$byClass['Sensitivity']
+    fp.rate[i] <- 1 - cm$byClass['Specificity']
+}
+
+# roc plot
+ggplot(data.frame(x = fp.rate,
+                  y = tp.rate), aes(x, y)) +
+    geom_line(color = 'royalblue') +
+    ggtitle("Support Vector Machine Testing ROC",
+            "Positive class = 2") +
+    xlab("False Positive Rate (1 - Specificity)") +
+    ylab("True Positive Rate (Sensitivity)")    
+
+################################################################################
+################################## MULTI-CLASS SVM #############################
+################################################################################
+
+set.seed(1)
+x <- rbind(x, matrix(rnorm(50*2), ncol = 2))
+y <- c(y, rep(0, 50))
+x[y==0] = x[y==0, 2] + 2
+dat = data.frame(x = x, y = as.factor(y))
+
+ggplot(dat, aes(x.1, x.2, color = y)) +
+    geom_point() +
+    ggtitle("Multi-Class SVM Data") +
+    xlab("X1") +
+    ylab("X2") +
+    labs(color = 'Class')
+
+svmfit <- svm(y ~ .,
+              data = dat,
+              kernel = 'radial',
+              cost = 10,
+              gamma = 1)
+plot(svmfit, dat)
+
+################################################################################
+############################# GENE EXPRESSION DATA #############################
+################################################################################
+
+suppressPackageStartupMessages(library(ISLR))
+names(Khan)
+dim(Khan$xtrain)
+dim(Khan$xtest)
+length(Khan$ytrain)
+length(Khan$ytest)
+
+table(Khan$ytrain)
+table(Khan$ytest)
+
+# training data
+dat <- data.frame(x = Khan$xtrain, y = factor(Khan$ytrain))
+out <- svm(y ~ .,
+           data = dat,
+           kernel = 'linear',
+           cost = 10)
+summary(out)
+table(out$fitted, dat$y)
+
+# testing data
+dat.e <- data.frame(x = Khan$xtest, y = factor(Khan$ytest))
+y.hat <- predict(out, newdata = dat.e)
+table(y.hat, dat.e$y)
